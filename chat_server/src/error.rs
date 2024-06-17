349071cd0901bac3f751beb17908c32391ff8d1d
lawff <evilllawliet@gmail.com>
2024-06-17 22:17:11 +0800

    fix: general error

--- chat_server/src/error.rs
+++ chat_server/src/error.rs
@@ -23,8 +23,8 @@ pub enum AppError {
     #[error("password hash error: {0}")]
     PasswordHashError(#[from] argon2::password_hash::Error),
 
-    #[error("jwt error: {0}")]
-    JwtError(#[from] jwt_simple::Error),
+    #[error("general error: {0}")]
+    AnyError(#[from] anyhow::Error),
 
     #[error("create chat error: {0}")]
     CreateChatError(String),
@@ -56,7 +56,7 @@ impl ErrorOutput {
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let status = match &self {
-            Self::JwtError(_) => StatusCode::FORBIDDEN,
+            Self::AnyError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::PasswordHashError(_) => StatusCode::UNPROCESSABLE_ENTITY,
             Self::SqlxError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::EmailAlreadyExists(_) => StatusCode::CONFLICT,
